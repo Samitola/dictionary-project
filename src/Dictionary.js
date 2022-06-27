00a2c341e1e3336aa0ae62fe55f0d7bf16d1cc14
Samitola <samitola986@gmail.com>
2022-06-27 09:24:03 -0400

    updated css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,24 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
   }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001c4fd3e3f50e240de9bfc2c6f33cc69a8";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then (handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -31,13 +42,19 @@ export default function Dictionary(props) {
     search();
   }
 
+  
+
   if (loaded) {
     return (
       <div className="Dictionary">
         <section>
           <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             suggested words: food, cocktails, rollerskates.......
